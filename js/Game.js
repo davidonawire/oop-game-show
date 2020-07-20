afa5aa00cd9ec1db9d1b53cfed8f2248437340c6
David Adams <onethinline@gmail.com>
2020-07-19 21:10:24 -0700

    Add createPhrases function

--- js/Game.js
+++ js/Game.js
@@ -5,15 +5,25 @@
  class Game {
      constructor() {
          this.missed = 0;
-         this.phrases = [];
+         this.phrases = this.createPhrases();
          this.activePhrase = null;
      }
-     
+
 
      startGame() {
 
      }
 
+
+     createPhrases() {
+        const phraseStrings = ['See ya later alligator',
+                    'A penny saved is a penny earned',
+                    'Dance like nobody is watching',
+                    'Wish you were here',
+                    'Measure twice cut once'];
+
+        return phraseStrings.map(phraseString => new Phrase(phraseString));
+     }
 
      getRandomPhrase() {
 
