938f4d66c24636e48119af6f1a167b2f9034b18f
David Adams <onethinline@gmail.com>
2020-07-21 12:21:05 -0700

    Add letter entry by way of keyboard

--- js/app.js
+++ js/app.js
@@ -14,4 +14,31 @@ document.getElementById('qwerty').addEventListener('click', (e) => {
     if (e.target.type === 'submit') {
         game.handleInteraction(e.target);
     }
-})
+});
+
+
+document.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    const matchExp = /^[a-z]$/i;
+
+    if ( matchExp.test(e.key) ) {
+        const button = getButtonByLetter(e.key);
+        game.handleInteraction(button);
+    }
+});
+
+
+/**
+ * Returns the key button which matches an alpha key
+ * @param   {string}    letter - the letter to match
+ * @returns {Node}      Key button
+ */
+function getButtonByLetter(letter) {
+    const keyButtons = document.querySelectorAll('.key');
+    
+    for (let button of keyButtons) {
+        if (button.textContent === letter.toLowerCase()) {
+            return button;
+        }
+    }
+}
